71ea77dbb7fca11d387fc939721e5e1dab5d83a6
Andrii Darmits <darmirs99@gmail.com>
2022-06-30 19:14:01 +0300

    footer implemented

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,85 @@
 import React from "react";
+import {
+  AddressColumn,
+  CarColumn,
+  Copyrights,
+  FooterContainer,
+  FooterInner,
+  FooterLink,
+  FooterLogo,
+  FooterLogoWrapper,
+  FooterSection,
+  FooterSocials,
+  PageColumn,
+  ServicesColumn,
+  Title,
+  UnderFooter,
+  WashAddress,
+  WashEmail,
+  WashTel,
+} from "./Footer.style";
+import Car_Wash_Logo from "../../assets/svgs/Car_Wash_Logo.svg";
+import footer_facebook from "../../assets/svgs/footer_facebook.svg";
+import footer_instagram from "../../assets/svgs/footer_instagram.svg";
+import footer_twitter from "../../assets/svgs/footer_twitter.svg";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterSection>
+      <FooterContainer>
+        <FooterInner>
+          <FooterLogoWrapper>
+            <FooterLogo src={Car_Wash_Logo} />
+          </FooterLogoWrapper>
+          <PageColumn>
+            <Title>Page</Title>
+            <FooterLink to="/">Privacy and Cookie Policy</FooterLink>
+            <FooterLink to="/">Search Term</FooterLink>
+            <FooterLink to="/">Orders and Returns</FooterLink>
+            <FooterLink to="/">Advanced Search</FooterLink>
+            <FooterLink to="/">Contact Us</FooterLink>
+          </PageColumn>
+          <CarColumn>
+            <Title>Car</Title>
+            <FooterLink to="/">Privacy and Cookie Policy</FooterLink>
+            <FooterLink to="/">Search Term</FooterLink>
+            <FooterLink to="/">Orders and Returns</FooterLink>
+            <FooterLink to="/">Advanced Search</FooterLink>
+            <FooterLink to="/">Contact Us</FooterLink>
+          </CarColumn>
+          <ServicesColumn>
+            <Title>Services</Title>
+            <FooterLink to="/">Privacy and Cookie Policy</FooterLink>
+            <FooterLink to="/">Search Term</FooterLink>
+            <FooterLink to="/">Orders and Returns</FooterLink>
+            <FooterLink to="/">Advanced Search</FooterLink>
+            <FooterLink to="/">Contact Us</FooterLink>
+          </ServicesColumn>
+          <AddressColumn>
+            <WashTel>
+              <a href="tel:(303) 985-0105">(303) 985-0105</a>
+              <span>, </span>
+              <a href="tel:(303) 985-0105">(303) 985-0105</a>
+            </WashTel>
+            <WashEmail href="mailto:mifs@info.com">mifs@info.com</WashEmail>
+            <WashAddress>
+              2972 Westheimer Rd. Santa Ana, Illinois 85486{" "}
+            </WashAddress>
+            <FooterSocials>
+              <a href="/">
+                <img src={footer_instagram} alt="instagram icon" />
+              </a>
+              <a href="/">
+                <img src={footer_facebook} alt="facebook icon" />
+              </a>
+              <a href="/">
+                <img src={footer_twitter} alt="twitter icon" />
+              </a>
+            </FooterSocials>
+          </AddressColumn>
+        </FooterInner>
+        <Copyrights>©2021 Car Wash. All Rights Reserved</Copyrights>
+      </FooterContainer>
+    </FooterSection>
+  );
 };
---
+++ src/components/Footer/Footer.style.js
@@ -0,0 +1,88 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { color, Container, device } from "../../GlobalStyles";
+export const FooterSection = styled.footer``;
+export const FooterContainer = styled(Container)`
+  padding-top: 60px;
+  padding-bottom: 70px;
+`;
+export const FooterInner = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 20%);
+
+  @media screen and (${device.tablet}) {
+    grid-template-columns: repeat(auto-fill, 50%);
+  }
+  @media screen and (max-width: 480px) {
+    grid-template-columns: repeat(auto-fill, 100%);
+    justify-items: center;
+  }
+  & > div {
+    margin-bottom: 20px;
+  }
+`;
+export const FooterLogoWrapper = styled.div``;
+export const FooterLogo = styled.img`
+  max-width: 165px;
+  width: 100%;
+  cursor: pointer;
+`;
+export const Title = styled.h5`
+  color: ${color.greyNavLink};
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  text-transform: uppercase;
+  margin-bottom: 26px;
+`;
+export const PageColumn = styled.div``;
+export const CarColumn = styled.div``;
+export const ServicesColumn = styled.div``;
+export const FooterLink = styled(Link)`
+  display: block;
+  font-size: 14px;
+  line-height: 26px;
+  color: ${color.white};
+`;
+export const AddressColumn = styled.address`
+  font-size: 16px;
+  line-height: 29px;
+  letter-spacing: 0.5px;
+  color: ${color.white};
+  margin-bottom: 10px;
+`;
+
+export const WashTel = styled.p`
+  opacity: 0.5;
+  margin-bottom: 20px;
+  & > a {
+    color: ${color.white};
+  }
+`;
+export const WashEmail = styled.a`
+  display: block;
+  color: ${color.white};
+  margin-bottom: 20px;
+`;
+export const WashAddress = styled.p`
+  opacity: 0.5;
+  margin-bottom: 18px;
+`;
+
+export const Copyrights = styled.div`
+  font-size: 14px;
+  line-height: 25px;
+  letter-spacing: 0.5px;
+`;
+export const FooterSocials = styled.div`
+  display: flex;
+  align-items: center;
+  transition: all 0.1s;
+  & > a {
+    margin-right: 65px;
+  }
+  & > a:hover {
+    display: block;
+    transform: scale(1.1);
+  }
+`;
